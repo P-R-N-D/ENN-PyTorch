77c2481ead5b3f1d474572d04b4780ea4cec9a3b
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-10-26 15:15:35 +0900

    Fix to call forward of the model explicitly

--- stnet/runtime/operation.py
+++ stnet/runtime/operation.py
@@ -1032,13 +1032,13 @@ def epoch(
                                     )
                                 except Exception:
                                     pass
-                            y_hat, loss_val = model.forward(
-                                X,
-                                labels_flat=Y_flat,
-                                net_loss=None,
-                                global_loss=top_loss,
-                                local_loss=bottom_loss,
-                                loss_weights=(alpha, beta),
+                            y_hat, loss_val = model(
+                                    X,
+                                    labels_flat=Y_flat,
+                                    net_loss=None,
+                                    global_loss=top_loss,
+                                    local_loss=bottom_loss,
+                                    loss_weights=(alpha, beta),
                             )
                             if step_idx == 0:
                                 try:
