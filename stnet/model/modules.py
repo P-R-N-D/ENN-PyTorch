68e1b0649741187990442dfe7fea3304091bf319
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-10-26 16:32:08 +0900

    Temporarily disabled some modules

--- stnet/model/modules.py
+++ stnet/model/modules.py
@@ -579,8 +579,8 @@ class Root(nn.Module):
                 kwargs = {k: v for k, v in kwargs.items() if k in allowed}
             return args, kwargs
 
-        self.register_forward_pre_hook(_sanitize_pre_hook, with_kwargs=True)
-        self._calib_pre_hook_handle = self._register_load_state_dict_pre_hook(
+        #self.register_forward_pre_hook(_sanitize_pre_hook, with_kwargs=True)
+        #self._calib_pre_hook_handle = self._register_load_state_dict_pre_hook(
             _calib_load_pre_hook, with_module=True
         )
         if config.device is not None:
