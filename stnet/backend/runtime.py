bf52fe480474a2b6141d4e99982dd3a0882b9d0c
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-11-13 19:23:52 +0900

    Update runtime.py

--- stnet/backend/runtime.py
+++ stnet/backend/runtime.py
@@ -97,6 +97,8 @@ except ImportError:
 
 
 ignored_sentences = [
+    "External init callback must run in same thread as registerClient*",
+    "Initializing zero-element tensors is a no-op*",
     "torch.distributed is disabled, unavailable or uninitialized, assuming the intent is to load in a single process.*",
     "torch.distributed is disabled, unavailable or uninitialized, assuming the intent is to save in a single process.*",
     "TypedStorage is deprecated.*",
