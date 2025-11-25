7cc3dec1ab7da854d398b3c55a240998e970c074
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-11-25 20:05:57 +0900

    Rearranging exception boundaries from compile

--- stnet/model/layers.py
+++ stnet/model/layers.py
@@ -1431,7 +1431,7 @@ class Processor(nn.Module):
         coords = self.spatial_coords_template.to(device=device, dtype=dtype)
         return coords.unsqueeze(0).expand(batch, -1, -1)
 
-    @torch_no_compile(reason='Safe from CUDAGraph error')
+    @torch_no_compile(reason='Safe from CUDAGraph error', recursive=False)
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         B = x.shape[0]
         spatial_raw = self.spatial_tokenizer(x)
