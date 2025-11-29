a72dc03c385e9f920a1d3a0dfe00d357d1f6d9df
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-11-29 20:50:18 +0900

    Update __init__.py

--- stnet/__init__.py
+++ stnet/__init__.py
@@ -17,8 +17,20 @@ __all__ = [
     "model",
 ]
 
+class IgnoreTorchCompileMsg(logging.Filter):
+    def filter(self, record):
+        msg = record.getMessage()
+        if "No valid triton configs" in msg:
+            return False
+        if "Runtime error during autotuning" in msg:
+            return False
+        if "Ignoring this choice" in msg:
+            return False
+        return True
 
 os.environ.setdefault("OPENCV_LOG_LEVEL", "SILENT")
 os.environ.setdefault("TORCH_CPP_LOG_LEVEL", "ERROR")
 logging.getLogger("torch.distributed").setLevel(logging.ERROR)
+logging.getLogger("torch._inductor").addFilter(IgnoreTorchCompileMsg())
+logging.getLogger("torch._inductor.select_algorithm").addFilter(IgnoreTorchCompileMsg())
 set_list_to_stack(True).set()
