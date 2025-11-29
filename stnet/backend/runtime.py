b7321a8e9a67bff5bd4b90b393c516fab3cb9bf9
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-11-29 23:41:03 +0900

    Update runtime.py

--- stnet/backend/runtime.py
+++ stnet/backend/runtime.py
@@ -2434,7 +2434,7 @@ def main(*args: Any, **kwargs: Any) -> Optional[Instance]:
             reduction="mean",
         )
         bottom_loss.base = LinearCombinationLoss(
-            coefficient=[0.7, 0.2, 0.1],
+            coefficient=[0.75, 0.1, 0.15],
             loss=[local_crps, local_z, local_t],
             reduce_each=False,
             auto_schedule=True,
