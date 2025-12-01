e4597c85c3c1be7ec90bf042fff3e163d24d31cb
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-12-01 17:07:14 +0900

    Update runtime.py

--- stnet/backend/runtime.py
+++ stnet/backend/runtime.py
@@ -2425,7 +2425,7 @@ def main(*args: Any, **kwargs: Any) -> Optional[Instance]:
             skew=ops.loss_skew,
         )
         top_loss = LinearCombinationLoss(
-            coefficient=[0.9, 0.1],
+            coefficient=[0.95, 0.05],
             loss=[top_df, top_z],
             reduce_each=False,
             auto_schedule=False,
