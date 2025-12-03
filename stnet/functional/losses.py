97cc1ed86662c2f1170a6040e1c4a620e6bc98bd
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-12-03 21:44:40 +0900

    Update losses.py

--- stnet/functional/losses.py
+++ stnet/functional/losses.py
@@ -1062,9 +1062,9 @@ class LinearCombinationLoss(nn.Module):
         offset: float = 0.0,
         reduce_each: bool = True,
         auto_schedule: bool = False,
-        schedule_momentum: float = 0.9,
-        min_coeff: float = 0.05,
-        max_coeff: float = 0.95,
+        schedule_momentum: float = 0.95,
+        min_coeff: float = 0.1,
+        max_coeff: float = 0.9,
         eps: float = 1e-06,
         **kwargs: Any,
     ) -> None:
@@ -1273,9 +1273,9 @@ class TiledLoss(nn.Module):
 
 @dataclass
 class LossWeightController:
-    momentum: float = 0.9
-    min_weight: float = 0.05
-    max_weight: float = 0.95
+    momentum: float = 0.95
+    min_weight: float = 0.1
+    max_weight: float = 0.9
     eps: float = 1e-06
     top_avg: float = 0.75
     bottom_avg: float = 0.25
