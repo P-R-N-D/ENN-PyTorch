3ce82574c3e7a38b92b69c10714929717c778f27
Priceless R&D <46338885+P-R-N-D@users.noreply.github.com>
2025-12-30 21:20:30 +0900

    Update __init__.py

--- stnet/__init__.py
+++ stnet/__init__.py
@@ -2,34 +2,50 @@
 from __future__ import annotations
 
 import importlib
+from typing import Any, Optional
 
-__all__ = ['core', 'data', 'nn', 'runtime', 'api', 'config', 'new_model', 'load_model', 'save_model', 'train', 'predict', 'get_prediction']
+from tensordict import TensorDictBase
 
-def __getattr__(name):
+from .nn.architecture import Model
+
+
+__all__ = [
+            'core', 'data', 'nn', 'runtime', 'api', 'config',
+            'new_model', 'load_model', 'save_model', 'train', 'predict', 'get_prediction',
+        ]
+
+
+def __getattr__(name: str) -> Optional[Any]:
     if name in {'core', 'data', 'nn', 'runtime', 'api', 'config'}:
         return importlib.import_module(f'stnet.{name}')
     raise AttributeError(f"module 'stnet' has no attribute '{name}'")
 
-def new_model(*args, **kwargs):
+
+def new_model(*args: Any, **kwargs: Any) -> Model:
     from .api import new_model as _new_model
-    return _new_model(*args, **kwargs)
+    return _new_model(*args: Any, **kwargs: Any)
 
-def load_model(*args, **kwargs):
+
+def load_model(*args: Any, **kwargs: Any) -> Model:
     from .api import load_model as _load_model
-    return _load_model(*args, **kwargs)
+    return _load_model(*args: Any, **kwargs: Any)
 
-def save_model(*args, **kwargs):
+
+def save_model(*args: Any, **kwargs: Any) -> None:
     from .api import save_model as _save_model
-    return _save_model(*args, **kwargs)
+    return _save_model(*args: Any, **kwargs: Any)
 
-def train(*args, **kwargs):
+
+def train(*args: Any, **kwargs: Any) -> Model:
     from .api import train as _train
-    return _train(*args, **kwargs)
+    return _train(*args: Any, **kwargs: Any)
 
-def predict(*args, **kwargs):
+
+def predict(*args: Any, **kwargs: Any) -> TensorDictBase:
     from .api import predict as _predict
-    return _predict(*args, **kwargs)
+    return _predict(*args: Any, **kwargs: Any)
 
-def get_prediction(*args, **kwargs):
+
+def get_prediction(*args: Any, **kwargs: Any) -> TensorDictBase:
     from .api import get_prediction as _get_prediction
-    return _get_prediction(*args, **kwargs)
+    return _get_prediction(*args: Any, **kwargs: Any)
