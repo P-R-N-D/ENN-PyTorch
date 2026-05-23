b13a3402c9e47429889c79778e4ab1b0d2224049
Kim Jeseok <46338885+P-R-N-D@users.noreply.github.com>
2026-05-23 09:31:36 +0900

    Export GlobalContextComposer

--- enn_torch_dev/nn/__init__.py
+++ enn_torch_dev/nn/__init__.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
-from .blocks import RegionCompressor
+from .blocks import (
+    GlobalContextComposer,
+    GlobalContextComposition,
+    RegionCompressor,
+)
 from .layers import AutoConvND, Reducer
 
 __all__ = [
     "AutoConvND",
+    "GlobalContextComposer",
+    "GlobalContextComposition",
     "Reducer",
     "RegionCompressor",
 ]
